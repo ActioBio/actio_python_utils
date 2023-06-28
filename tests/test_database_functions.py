0d174d153c48b195948d5f48775e9b417d6508c9
Brett Copeland <brcopeland@gmail.com>
2023-06-28 10:57:47 -0700

    Added more unit tests of db_args.

--- tests/test_database_functions.py
+++ tests/test_database_functions.py
@@ -1,16 +1,43 @@
-import unittest
+import os
 from psycopg2 import OperationalError
+from unittest import main, mock, TestCase
 from actio_python_utils import database_functions as df
 
 
-class test_get_db_args(unittest.TestCase):
+class test_get_db_args(TestCase):
     def test_service(self):
         self.assertEqual(
-            df.get_db_args(service="bioinfo_data_psql")["service"], "bioinfo_data_psql"
+            df.get_db_args(service="apple")["service"], "apple"
         )
 
+    def test_service_and_db_args(self):
+        with self.assertRaises(ValueError):
+            df.get_db_args(
+                service="bioinfo_data_psql", db_args={"service": "bioinfo_data_psql"}
+            )
 
-class test_db_connection_db_args(unittest.TestCase):
+    @mock.patch.dict(
+        os.environ,
+        {
+            "DB_CONNECTION_STRING": "postgres://your_user:your_password@your_host:your_port/your_database",
+            "PGSERVICE": "xyz",
+        },
+    )
+    def test_db_connection_string(self):
+        self.assertEqual(
+            df.get_db_args()["dsn"],
+            "postgres://your_user:your_password@your_host:your_port/your_database",
+        )
+
+    @mock.patch.dict(os.environ, {"PGSERVICE": "abc"})
+    def test_pgservice(self):
+        self.assertEqual(df.get_db_args()["service"], "abc")
+
+    def test_cfg(self):
+        self.assertEqual(df.get_db_args()["service"], "bioinfo_data_psql")
+
+
+class test_db_connection_db_args(TestCase):
     def setUp(self):
         self.db = df.DBConnection(service="bioinfo_data_psql")
 
@@ -18,7 +45,7 @@ class test_db_connection_db_args(unittest.TestCase):
         self.assertEqual(self.db.db_args["service"], "bioinfo_data_psql")
 
 
-class test_db_connection(unittest.TestCase):
+class test_db_connection_service(TestCase):
     def setUp(self):
         self.db = df.DBConnection(service="bioinfo_data_psql")
 
@@ -33,4 +60,4 @@ class test_db_connection(unittest.TestCase):
 
 
 if __name__ == "__main__":
-    unittest.main()
+    main()
